7a85898a01d86a99252c132e0837b93e35e3240d
mmx <965193934@qq.com>
2026-05-08 08:26:46 +0800

    数组的练习 	new file:   复习_数组求和.c 	new file:   练习_数组.c 	deleted:    README.md 	modified:   重写_02_basic_types.c

---
+++ 复习_数组求和.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum = 0;
+    int a = 1;
+
+    scanf("%d", &n)
+
+        for (int i = 1; i <= n; i++) {
+            sum += i * a;
+            a = -a
+        }
+    printf("%d", sum)
+
+        return 0;
+}
